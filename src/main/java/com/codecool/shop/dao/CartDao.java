539ad48055c3b837a22b63f912d303d02da28b93
András Rozner <roznernek@gmail.com>
2019-08-28 13:39:39 +0200

    cart finished fully

--- src/main/java/com/codecool/shop/dao/CartDao.java
+++ src/main/java/com/codecool/shop/dao/CartDao.java
@@ -3,10 +3,12 @@ package com.codecool.shop.dao;
 import com.codecool.shop.model.Product;
 
 import java.util.List;
+import java.util.Map;
 
 public interface CartDao {
 
     void addToCart(Product product);
-    List<Product> getCart();
+    Map<Product,Integer> getCart();
     void removeFromCart(Product product);
+    float getSumOfPrices();
 }
