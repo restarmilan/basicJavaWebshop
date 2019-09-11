fe2179486eb5c22f21074376e3e13118033d9791
András Rozner <roznernek@gmail.com>
2019-09-11 13:49:39 +0200

    Login without hashed password validation

--- src/main/java/com/codecool/shop/controller/LoginController.java
+++ src/main/java/com/codecool/shop/controller/LoginController.java
@@ -4,6 +4,8 @@ package com.codecool.shop.controller;
 import com.codecool.shop.dao.implementation.CartDaoMem;
 import com.codecool.shop.dao.implementation.UserDaoJdbcMem;
 import com.codecool.shop.model.User;
+import org.json.JSONObject;
+import org.json.JSONTokener;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -11,6 +13,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import java.io.BufferedReader;
 import java.io.IOException;
 
 @WebServlet(urlPatterns = {"/login"})
@@ -19,8 +22,11 @@ public class LoginController extends HttpServlet {
 
     @Override
     protected void doPost (HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        String username = req.getParameter("username");
-        String password = req.getParameter("password");
+        BufferedReader reader = req.getReader();
+        JSONTokener tokener = new JSONTokener(reader);
+        JSONObject json = new JSONObject(tokener);
+        String username = json.getString("username");
+        String password = json.getString("password");
         User user = UserDaoJdbcMem.getInstance().getUsernameAndPassword(username);
         if(user != null){
             if(user.getPassword().equals(password)){
