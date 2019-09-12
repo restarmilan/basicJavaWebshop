6923dec0e9200f2567f17e5316365e5a7f70cc9a
restarmilan <restarmilan@gmail.com>
2019-09-12 13:31:18 +0200

    Add basic Junit tests for ProductCategoryDaoMem class'methods.

---
+++ src/tests/com/codecool/shop/dao/implementation/ProductCategoryDaoMemTest.java
@@ -0,0 +1,48 @@
+package com.codecool.shop.dao.implementation;
+
+import com.codecool.shop.model.ProductCategory;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+class ProductCategoryDaoMemTest {
+
+    private ProductCategoryDaoMem productCategoryDaoMem = ProductCategoryDaoMem.getInstance();
+
+    @Test
+    public void testIsAddingANewProductCategory() {
+        ProductCategory testCategory = new ProductCategory("testname", "testdepartment", "category only for testing");
+        ProductCategory otherTestCategory = new ProductCategory("otherTestName", "testdepartment", "category only for testing");
+        productCategoryDaoMem.add(testCategory);
+        productCategoryDaoMem.add(otherTestCategory);
+        assertEquals(testCategory, productCategoryDaoMem.find(1));
+        assertEquals(otherTestCategory, productCategoryDaoMem.find(2));
+    }
+
+    @Test
+    public void testIsFindCategoryByAGivenId() {
+        ProductCategory category = productCategoryDaoMem.find(1);
+        assertEquals("testname", category.getName());
+        assertEquals("testdepartment", category.getDepartment());
+    }
+
+    @Test
+    public void testIsRemovingByAGivenId() {
+        productCategoryDaoMem.remove(2);
+        assertNull(productCategoryDaoMem.find(2));
+    }
+
+    @Test
+    public void testIsGiveBackAllCategories() {
+        List<ProductCategory> testList = new ArrayList<>();
+        //ProductCategory testCategory = new ProductCategory("testname", "testdepartment", "category only for testing");
+        testList.add(productCategoryDaoMem.find(1));
+        testList.add(productCategoryDaoMem.find(2));
+        assertEquals(testList, productCategoryDaoMem.getAll());
+    }
+
+}
