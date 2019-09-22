aca413dfc54e74dfcbbc568264a0eff547516ef9
restarmilan <restarmilan@gmail.com>
2019-09-22 13:16:42 +0200

    First encounters with slf4j logging

--- src/main/java/com/codecool/shop/controller/DatabaseController.java
+++ src/main/java/com/codecool/shop/controller/DatabaseController.java
@@ -1,16 +1,11 @@
 package com.codecool.shop.controller;
 
-import com.codecool.shop.dao.ProductCategoryDaoJdbc;
-import com.codecool.shop.dao.ProductDaoJdbc;
-import com.codecool.shop.dao.SupplierDaoJdbc;
-import com.codecool.shop.dao.implementation.*;
-import com.codecool.shop.model.Product;
-import com.codecool.shop.model.ProductCategory;
-import com.codecool.shop.model.Supplier;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-import java.sql.*;
-import java.lang.*;
-import java.util.List;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class DatabaseController {
 
@@ -18,6 +13,7 @@ public class DatabaseController {
     private String DATABASE = "jdbc:postgresql://localhost:5432/codecoolshop";
     private String DB_USER = System.getenv("DB_USER");
     private String DB_PASSWORD = System.getenv("DB_PASSWORD");
+    private static final Logger logger = LoggerFactory.getLogger(DatabaseController.class);
 
     public DatabaseController() {}
 
@@ -29,12 +25,15 @@ public class DatabaseController {
 
     public Connection getConnection() {
         try {
+            logger.info("Connection created successfully");
             return DriverManager.getConnection(
                     DATABASE,
                     DB_USER,
                     DB_PASSWORD);
+
         } catch (SQLException e) {
             e.printStackTrace();
+            logger.error("Connection with database failed", e);
         }
         return null;
     }
